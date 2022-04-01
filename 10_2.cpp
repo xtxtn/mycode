7474655a65fa0316d991af0c966249743b91eb6c
xtxnb <94107819+xtxnb@users.noreply.github.com>
2022-04-01 12:12:35 +0800

    Create 10_2.cpp

---
+++ 10_2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+class Person{
+private:
+    static const int LIMIT=25;
+    string lname;
+    char fname[LIMIT];
+public:
+    Person(){lname="";fname[0]='\0';}
+    Person(const string &ln,const char *fn="Heyyou");
+    void Show() const;
+    void FormalShow() const;
+};
+Person::Person(const string &ln,const char *fn){
+    lname=ln;
+    strcpy(fname,fn);
+}
+void Person::Show() const{
+    cout<<"last:"<<lname<<endl;
+}
+void Person::FormalShow() const{
+    cout<<"first:"<<fname<<endl;
+}
+int main(){
+    Person one;
+    Person two("Smythecraft");
+    Person three("Dimwiddy","Sam");
+    one.Show();
+    one.FormalShow();
+    two.Show();
+    two.FormalShow();
+    three.Show();
+    three.FormalShow();
+}
