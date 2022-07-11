ee01a75d609b8c07b8b36b3270211a7b591158df
xtxnb <94107819+xtxnb@users.noreply.github.com>
2022-07-11 09:37:08 +0800

    Update search-insert-position.cpp

--- leetcode/search-insert-position.cpp
+++ leetcode/search-insert-position.cpp
@@ -17,3 +17,23 @@ public:
         return left ;
     }
 };
+/*
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0 , right = nums.size()  , middle;
+        while (left < right)
+        {
+            middle = (left + right) / 2;
+            if (nums[middle] > target){
+                right = middle ;
+            }
+            else if(nums[middle] < target){
+                left = middle + 1;
+            }
+            else
+                return middle;
+        }
+        return left ;
+    }
+};*/
