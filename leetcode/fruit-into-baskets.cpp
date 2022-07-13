2fdd1ed33226b9d3718e7f91f9f5f3b538013245
xtxnb <94107819+xtxnb@users.noreply.github.com>
2022-07-13 17:23:33 +0800

    Update fruit-into-baskets.cpp

--- leetcode/fruit-into-baskets.cpp
+++ leetcode/fruit-into-baskets.cpp
@@ -6,27 +6,27 @@ public:
         int length = 0 ,result = 0;
         //m = fruits[j++];
         while (1){
-            m = fruits[i];
-            while ( m == fruits[j] || n == fruits[j]){
+            m = fruits[i];                              //找到第一种水果
+            while ( m == fruits[j] || n == fruits[j]){  //找到第二种水果
                 if (j == fruits.size() - 1)
                     break;
                 j ++;
             }
             n = fruits[j];
-            t = j;
-            while ( m == fruits[j] || n == fruits[j] ){
+            t = j;   //记录第二种水果                        
+            while ( m == fruits[j] || n == fruits[j] ){ //找到第三种水果时停止
                 if (j == fruits.size() - 1)
                     break;
                 j ++;
             }
-            if ( m == fruits[j] || n == fruits[j])
+            if ( m == fruits[j] || n == fruits[j])      //判断是否到达边界
                 length = j - i + 1;
             else
                 length = j - i;
             result = result > length ? result :length;
-            if (j == fruits.size() - 1) 
+            if (j == fruits.size() - 1)                //跳出循环
             break;           
-            i = t;
+            i = t;                                     //初始化水果
             j = t;
         }
         return result;
