9be012d6117bff1d681c5fd6f4da581fdfc681d6
xtxnb <94107819+xtxnb@users.noreply.github.com>
2022-07-13 17:28:15 +0800

    Update minimum-size-subarray-sum.cpp

--- leetcode/minimum-size-subarray-sum.cpp
+++ leetcode/minimum-size-subarray-sum.cpp
@@ -6,11 +6,11 @@ public:
         int result = INT32_MAX;
         int sum = 0;
         for ( ; j < nums.size() ;j ++){
-            sum += nums[j];
+            sum += nums[j];                 // j左移，窗口变大
             while (sum >= target ){
-                length = j - i + 1;
+                length = j - i + 1;         
                 result = length < result ? length : result;
-                sum -= nums[i++];
+                sum -= nums[i++];           // i左移，窗口变小
             }
         }
         return result = result < INT32_MAX ? result : 0;
