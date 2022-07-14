225070b0961c0be5f4eee739a96e8d8098f498b9
xtxnb <94107819+xtxnb@users.noreply.github.com>
2022-07-14 10:19:26 +0800

    Update fruit-into-baskets.cpp

--- leetcode/fruit-into-baskets.cpp
+++ leetcode/fruit-into-baskets.cpp
@@ -1,6 +1,31 @@
 //904.水果成篮
 class Solution {
 public:
+    int totalFruit(vector<int>& fruits) {       //滑动窗口解法
+        int i = 0 , j = 0;
+        vector<int> obj(fruits.size(),0);     //记录每种水果的的个数
+        int type = 1;                         //初始化水果种数
+        int sum = 1 , result = 0;
+        obj[fruits[i]] = 1;                 
+        
+        for (j = 1; j < fruits.size() ; j ++){
+            if ( !(obj[fruits[j]]))         //如果这种水果个数为0，则表示该种水果未采摘，水果种数 +1
+                type ++;                    
+            obj[fruits[j]] ++;              
+            sum ++;
+            while (type > 2){
+                obj[fruits[i]] --;          //放回水果
+                sum --;
+                if ( !(obj[fruits[i]])){   //当放回该种该种水果为0时即可
+                    type --;
+                }
+                i ++;
+            }
+            result = max(sum , result);
+        }
+        return  result = max(sum , result);
+    }
+    /*
     int totalFruit(vector<int>& fruits) {
         int m = -1, n = -1, i = 0 , j = 0 ,t;
         int length = 0 ,result = 0;
@@ -30,5 +55,5 @@ public:
             j = t;
         }
         return result;
-    }
+    }*/
 };
